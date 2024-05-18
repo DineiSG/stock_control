bc62e0bcc1ab7f2ae9b2f499b8ddc268370cc37d
Dinei <waldinei.wsg@gmail.com>
2024-05-18 00:01:27 -0300

    Adicionados os icones

--- src/components/Acesso.js
+++ src/components/Acesso.js
@@ -4,25 +4,29 @@ import styles from './Acesso.module.css'
 const Acesso = () => {
   return (
     <div>
-      <div className='container-md'>
+      <div className={styles.box}>
         <img className={styles.logo} src='/logo2.png' alt='logo'/>
-        <form className={styles.form}>
-          <p>Login</p>
-          <div className={styles.group}>
-            <input required="true" className={styles.main_input} type="text" />
-            <span className={styles.highlight_span}></span>
-            <label className={styles.lebal_email}>CPF:</label>
-          </div>
-          <div className={styles.container_1}>
+        
+          <form className={styles.form}>
+            <p><svg xmlns="http://www.w3.org/2000/svg" width="140" height="80" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+            <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+            </svg>Login</p>
             <div className={styles.group}>
               <input required="true" className={styles.main_input} type="text" />
               <span className={styles.highlight_span}></span>
-              <label className={styles.lebal_email}>Senha:</label>
+              <label className={styles.lebal_email}>CPF:</label>
             </div>
-          </div>
-          <button className={styles.submit}>Acessar</button>
-        </form>
-      </div>
+            <div className={styles.container_1}>
+              <div className={styles.group}>
+                <input required="true" className={styles.main_input} type="text" />
+                <span className={styles.highlight_span}></span>
+                <label className={styles.lebal_email}>Senha:</label>
+              </div>
+            </div>
+            <button className={styles.submit}>Acessar</button>
+          </form>
+        </div>
     </div>
   )
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -14,8 +14,10 @@ const Filter = () => {
         <div className={styles.container}>
           <h2>Selecione um filtro:</h2>
           <div className={styles.select}>
-              <button className={styles.filtrar} onClick={()=>setFiltroLoja(!filtroLoja)}>
-                {filtroLoja?'Por Loja':'Filtrar por Loja'}</button>
+              <button className={styles.filtrar} onClick={()=>setFiltroLoja(!filtroLoja)}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="25" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+              <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
+              </svg>
+              {filtroLoja?'Por Loja':'Por Loja'}</button>
               {filtroLoja ? 
                 <table className="table table-success">
                   <thead>
@@ -39,7 +41,10 @@ const Filter = () => {
                       </tr>
                   </tbody>
                 </table> : null }
-              <button className={styles.filtrar} onClick={()=>setFiltroMarca(!filtroMarca)}>{filtroMarca?'Por Marca':'Filtrar por Marca'}</button>
+              <button className={styles.filtrar} onClick={()=>setFiltroMarca(!filtroMarca)}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="25" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+              <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
+              </svg>
+              {filtroMarca?'Por Marca':'Por Marca'}</button>
               {filtroMarca ? 
                 <table className="table table-success">
                   <thead>
@@ -63,7 +68,10 @@ const Filter = () => {
                     </tr>
                   </tbody>
               </table> : null }
-              <button className={styles.filtrar} onClick={()=>setFiltroModelo(!filtroModelo)}>{filtroModelo? 'Por Modelo':'Filtrar por Modelo'}</button>
+              <button className={styles.filtrar} onClick={()=>setFiltroModelo(!filtroModelo)}><svg xmlns="http://www.w3.org/2000/svg" width="35" height="25" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+              <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
+              </svg>
+              {filtroModelo? 'Por Modelo':'Por Modelo'}</button>
               {filtroModelo? 
                 <table className="table table-success">
                   <thead>
